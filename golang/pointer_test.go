8fee585f8c0f93e4e10b0229e691f405f9f2f650
gofree-wtf <gofree.wtf@gmail.com>
2021-02-12 10:17:55 +0900

    Fix pointer_test.go

--- golang/pointer_test.go
+++ golang/pointer_test.go
@@ -1,40 +1,34 @@
 package main
 
 import (
-	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"unsafe"
 )
 
-type ABC struct {
-	test int
+type People struct {
+	Name string
 }
 
-func (a *ABC) Frame() {
-	fmt.Println("------------")
-	fmt.Printf("addr of ptr: %p\n", a)
-
-	fmt.Print("addr of ptr: ")
-	fmt.Println(unsafe.Pointer(a))
-
-	fmt.Print("addr of double ptr: ")
-	fmt.Println(&a)
-
-	fmt.Print("addr of double ptr: ")
-	fmt.Println(unsafe.Pointer(&a))
-
-	var b **ABC = &a
-
-	fmt.Print("addr of double ptr: ")
-	fmt.Println(b)
-
-	fmt.Print("addr of double ptr: ")
-	fmt.Println(unsafe.Pointer(b))
-}
-
-func TestPointer(t *testing.T) {
-	test := &ABC{1}
-	for i := 0; i < 10; i++ {
-		test.Frame()
+func TestPointer2(t *testing.T) {
+	var instance = People{
+		Name: "gofree",
 	}
+	log.WithField("instance", instance).Info("print instance")
+	log.WithField("address", unsafe.Pointer(&instance)).Info("addr of instance") // ex) 0x0a
+
+	var ptr *People = nil
+	log.WithField("value", unsafe.Pointer(ptr)).Info("value of nil ptr")   // nil
+	log.WithField("address", unsafe.Pointer(&ptr)).Info("addr of nil ptr") // ex) 0x01
+
+	ptr = &instance
+	log.WithField("value", unsafe.Pointer(ptr)).Info("value of ptr")   // ex) 0x0a
+	log.WithField("address", unsafe.Pointer(&ptr)).Info("addr of ptr") // ex) 0x01
+	assert.Equal(t, unsafe.Pointer(&instance), unsafe.Pointer(ptr))
+
+	var doublePtr **People = &ptr
+	log.WithField("value", unsafe.Pointer(doublePtr)).Info("value of double ptr")   // ex) 0x01
+	log.WithField("address", unsafe.Pointer(&doublePtr)).Info("addr of double ptr") // ex) 0x02
+	assert.Equal(t, unsafe.Pointer(&ptr), unsafe.Pointer(doublePtr))
 }
